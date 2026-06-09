93b8a0775bd111dd7efc7a566a5b6a0b9a7812ab
Julian Mesa <jdmesalosada@gmail.com>
2026-06-09 06:31:56 -0500

    Reto 5 y 6 completado

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -1,8 +1,8 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test('Check left menu options', async({page}) => {
+test('Check left menu options', async ({ page }) => {
 
-    
+
     await page.goto('https://opensource-demo.orangehrmlive.com/')
     await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
     await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
@@ -16,7 +16,7 @@ test('Check left menu options', async({page}) => {
 
     const currentMenuItems: string[] = []
 
-    for(let i=0; i<currentMenuItemsCount; i++){
+    for (let i = 0; i < currentMenuItemsCount; i++) {
 
         const menuText = await leftMenuItems.nth(i).innerText()
         currentMenuItems.push(menuText)
@@ -24,7 +24,7 @@ test('Check left menu options', async({page}) => {
 
     console.log(currentMenuItems)
 
-      const expectedMenuItems = [
+    const expectedMenuItems = [
         'Admin',
         'PIM',
         'Leave',
@@ -41,4 +41,70 @@ test('Check left menu options', async({page}) => {
 
     expect(currentMenuItems).toEqual(expectedMenuItems)
 
+})
+
+test('Navigate through the left panel', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+    const leftMenuItems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemsCount = await leftMenuItems.count()
+
+    for (let i = 0; i < currentMenuItemsCount; i++) {
+        const menuItem = leftMenuItems.nth(i)
+        const menuText = await menuItem.innerText()
+
+        console.log('Current menu item', menuText)
+
+        if (menuText !== 'Maintenance') {
+            await menuItem.click()
+        }
+    }
+})
+
+test('Check all the qualification links', async({page}) => {
+
+    const expectedPages = [
+        {
+            menu: 'Skills',
+            url: '/web/index.php/admin/viewSkills'
+        },
+        {
+            menu: 'Education', 
+            url: '/web/index.php/admin/viewEducation'
+        },
+        {
+            menu: 'Licenses', 
+            url: '/web/index.php/admin/viewLicenses'
+        },
+        
+    ]
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+    await page.getByRole('link', { name: 'Admin' }).click()
+
+    await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Qualifications').click()
+
+    const qualificationOptions = page.getByRole('menu').locator('li')
+
+    for(let expectedPage of expectedPages){
+
+        const menuOption = qualificationOptions.filter({hasText: expectedPage.menu})
+        await menuOption.click()
+        await expect(page).toHaveURL(new RegExp(expectedPage.url))
+
+        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('Qualifications').click()
+
+    } 
 })
--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -32,3 +32,39 @@ test('Get all the usernames registered', async ({ page }) => {
     console.log(usernames)
 
 })
+
+
+test('Select specific user for edition', async ({ page }) => {
+
+    const userForEdition = 'Belinda_Leuschke5123'
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+    await page.getByRole('link', { name: 'Admin' }).click()
+
+    await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+    await page.getByRole('menuitem', { name: 'Users' }).click()
+
+    const pencilToEdit = page
+        .getByRole('table')
+        .getByRole('row')
+        .filter({ hasText: userForEdition })
+        .locator('button')
+        .filter({ has: page.locator('i.bi-pencil-fill') })
+
+
+    await pencilToEdit.click()
+
+    const currentUsername = await page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input")
+    .inputValue()
+
+    expect(currentUsername).toEqual(userForEdition)
+
+    expect(page.locator("//label[contains(., 'Username')]/parent::div/following-sibling::div/input"))
+     .toHaveValue(currentUsername)    
+})
